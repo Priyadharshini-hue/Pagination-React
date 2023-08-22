e7a2acac1e009157b3c8bb1f7e6ba7283f859750
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-08-22 16:51:14 +0530

    update next/prev button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,39 @@
 import React from "react";
+import "./Button.css";
 
-const Button = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNos = [];
+const Button = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+  const pageNumbers = [];
+
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNos.push(i);
-    // console.log(i);
+    pageNumbers.push(i);
   }
+
   return (
-    <div id="button">
-      {pageNos.map((number) => (
-        <button key={number} onClick={() => paginate(number)} className="btn">
-          {number}
-        </button>
-      ))}
-    </div>
+    <nav className="nav">
+      <ul className="pagination">
+        <li>
+          <button
+            onClick={() => paginate(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+        </li>
+        {pageNumbers.map((number) => (
+          <li key={number}>
+            <button onClick={() => paginate(number)}>{number}</button>
+          </li>
+        ))}
+        <li>
+          <button
+            onClick={() => paginate(currentPage + 1)}
+            disabled={currentPage === pageNumbers.length}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
